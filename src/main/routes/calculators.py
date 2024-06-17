2dfe5e32c2fe77260711da57ffd7206acd8dd196
Anderson Suga <anderson.suga@gmail.com>
2024-06-17 20:46:15 -0300

    Finalizando Primeira Calculadora

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -7,5 +7,5 @@ calc_route_bp = Blueprint("calc_routes", __name__)
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
     calc = Calculator1()
-    calc.calculate(request)
-    return jsonify({"success": True}), 200
+    response = calc.calculate(request)
+    return jsonify(response), 200
